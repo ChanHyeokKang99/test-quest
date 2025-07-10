d269b42e316b020fec669d96b1af42b7b6f8d7f6
KChanHyeok <kang9kang9@gmail.com>
2025-07-11 00:42:06 +0900

    조회 수정

--- src/main/java/com/justteam/test_quest_api/api/gameboard/repository/GameBoardRepository.java
+++ src/main/java/com/justteam/test_quest_api/api/gameboard/repository/GameBoardRepository.java
@@ -26,7 +26,7 @@ public interface GameBoardRepository extends JpaRepository<GameBoard, String> {
     @Query("SELECT new com.justteam.test_quest_api.api.gameboard.dto.GameBoardSummaryDto(g.id, g.title, g.description, g.platform, g.type, g.thumbnailUrl, g.linkUrl, g.startDate, g.endDate, g.author, g.views, g.createAt, g.recruitStatus, g.user.nickname) " +
             "FROM GameBoard g " +
             "WHERE (:keyword IS NULL OR :keyword = '' OR g.title LIKE %:keyword% OR CAST(g.user.userId AS string) LIKE %:keyword%) " + // g.user.userId -> g.userId (일반적인 경우)
-            "AND (g.createAt, g.id) > (:lastCreateAt, :lastId) " + // 오름차순 커서 조건
+            "AND (g.createAt > :lastCreateAt OR (g.createAt = :lastCreateAt AND g.id > :lastId)) " + // 오름차순 커서 조건
             "ORDER BY g.createAt ASC, g.id ASC") // 정렬 기준도 커서와 일치
     List<GameBoardSummaryDto> findNextPageByCreateAtAscAndOptionalKeyword( // <-- 반환 타입 GameBoard로 변경
                                                                            @Param("keyword") String keyword,
@@ -36,7 +36,7 @@ public interface GameBoardRepository extends JpaRepository<GameBoard, String> {
     @Query("SELECT new com.justteam.test_quest_api.api.gameboard.dto.GameBoardSummaryDto(g.id, g.title, g.description, g.platform, g.type, g.thumbnailUrl, g.linkUrl, g.startDate, g.endDate, g.author, g.views, g.createAt, g.recruitStatus, g.user.nickname) " +
             "FROM GameBoard g " +
             "WHERE (:keyword IS NULL OR :keyword = '' OR g.title LIKE %:keyword% OR CAST(g.user.userId AS string) LIKE %:keyword%) " + // g.user.userId -> g.userId (일반적인 경우)
-            "AND (g.createAt, g.id) < (:lastCreateAt, :lastId) " + // 내림차순 커서 조건
+            "AND (g.createAt < :lastCreateAt OR (g.createAt = :lastCreateAt AND g.id > :lastId)) " + // 내림차순 커서 조건
             "ORDER BY g.createAt DESC, g.id DESC") // 정렬 기준도 커서와 일치
     List<GameBoardSummaryDto> findNextPageByCreateAtDescAndOptionalKeyword( // <-- 반환 타입 GameBoard로 변경
                                                                   @Param("keyword") String keyword,
